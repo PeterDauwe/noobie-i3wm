7d5b356ae192a7a857c2aa7aaa00106daceb5a26
PeterDauwe <bobo5290461@gmail.com>
2021-03-30 18:28:28 +0200

    up

--- etc/skel/.config/i3/scripts/blurlock.sh
+++ etc/skel/.config/i3/scripts/blurlock.sh
@@ -6,10 +6,10 @@ DIR="/usr/share/backgrounds/*"
 SCREEN=$(ls $DIR/*.jpg | shuf -n1)
 
 # Convert wallpaper to blurred picture and put it in the i3lock dir
-convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) $SCREEN -blur 0x4 /home/$USER/.cache/bluredlock/blurlock.png &&\
+convert -resize $(xdpyinfo | grep dimensions | cut -d\  -f7 | cut -dx -f1) $SCREEN -blur 0x4 ~/.config/i3/bluredlock/blurlock.png &&\
 
 #Use the blurred picture in the lock screen
-i3lock -i /home/$USER/.cache/bluredlock/blurlock.png
+i3lock -i ~/.config/i3/bluredlock/blurlock.png
 
 #After unlock remove the blurred picture
 #rm /home/$USER/.cache/i3lock/blurlock.png
